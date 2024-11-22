fda62437b879e68d01d5bd35f530deb46a86f7ce
Eoin Groat <iam@eoingroat.com>
2024-11-22 15:18:53 +0000

    Partially tidies the processing of the word list. Most imprtantly this doesn't add Nones to the set

--- user_vocab.py
+++ user_vocab.py
@@ -14,8 +14,25 @@ def remove_non_character(word):
     edited_word = [character for character in word if ord(character) in range (97, 123)]
     if len("".join(edited_word)) > 0:
         return "".join(edited_word) 
+    # This was implicit before; its not great to have a function `(str) -> str | None``
+    return None
 
-unclean_vocab = sorted({words.lower() for words in content if " " not in words}) #issue here: as single words with spaces excluded
-clean_vocab = {remove_non_character(word) for word in unclean_vocab}
+# nb. sort was not necessary
+
+clean_vocab = set()
+for line in content:
+
+    # Removes any leading or trailing whitespace
+    line = line.strip()
+
+    if " " in line:
+        # Skip any lines that contain spaces, as they are probably not single words
+        continue
+
+    # sanitizes the word by removing any non-alphabetic characters and converting to lowercase
+    word = remove_non_character(line.lower())
+
+    if word is not None:
+        clean_vocab.add(word)
 
 #print("This is clean_vocab: ", clean_vocab)
