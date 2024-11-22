71504324da9b98855dba8ec311e6df6dd347a86c
Eoin Groat <iam@eoingroat.com>
2024-11-22 16:01:40 +0000

    This file gives an opportunity to learn a lot about structuring code and handling errors.
    The key issue you are seeing is the code goes really slow because its calling the API loads.

--- get_defs.py
+++ get_defs.py
@@ -1,5 +1,9 @@
 import requests
 
+# In this file, focus on:
+# - error handling as a concept
+# - not calling your most expensive API function so frequently
+
 def api_call(word: str) -> list:
     
     url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
@@ -10,6 +14,9 @@ def api_call(word: str) -> list:
             #print(f"Data found for {word}")
             return data
     else:
+            # Silent failure like this is a bad idea.
+            # Look into exceptions and error handling.
+            raise Exception(f"non-200 status code returned for {word}: {response.status_code}")
             #print(f"Bad Request - no relvant url found for {word}")
             return None
     
@@ -17,16 +24,20 @@ def api_call(word: str) -> list:
 #Returns a set of all found vocab types for a given word (e.g. {"verb", "noun"})
 def word_types(word):
     
-    word_types = set({})
+    word_types = set({}) # set() is a better way to create an empty set. set({}) is creating a set by iterating over an empty dictionary.
 
     try:
+        # Using error handling simplifies this call
         if api_call(word) != None:
+            # Oh no! we are calling the API with the exact same word twice in rapid succession; its better not to do that.
             for items in api_call(word):
-                for mean_items in items['meanings']:
+                for mean_items in items['meanings']: # Naming
                     word_types.add(mean_items['partOfSpeech'])
-            if len(list(word_types)) > 0:
+            if len(list(word_types)) > 0: # You can just len(word_types) - it is a set.
                 return word_types
+            
     except:
+        # Is it better to return None, the empty set, or raise an exception?
         return None
                     
 
@@ -61,5 +72,6 @@ def extract_defs(word, vocab_type = None):
     
 
     else:
+        # 👀
         return None
 
