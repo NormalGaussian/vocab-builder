b6343a0824820608116358e40ee3b3aef30c33ce
Eoin Groat <iam@eoingroat.com>
2024-11-22 16:08:13 +0000

    Final top level comments

--- vocab_game.py
+++ vocab_game.py
@@ -7,12 +7,18 @@ from random import sample
 from random import choice
 from set_choose import setChoice
 
+# Note that this function doesn't really do what it says on the tin
 def check_correct():
     """
     Checks to see if a given word has a url associated
     Checks to see if a given word has vocab_types associated
     Generates a new word if not
     """
+
+    # This looping structure is causing the code to run slow.
+    # The more words have no word_types, the slower the code will run.
+    # Any kind of loop like this can be considered a potential infinite loop; we should have a way to guarantee it will end.
+    # The most ideal guarantee is one that ends in constant time - as then our program will remain fast.
 
     answer = setChoice(clean_vocab)
 
@@ -29,7 +35,7 @@ def vocab_choices():
     
     vocab_selection = []
     answer = check_correct()
-    answer_type = choice(list(word_types(answer)))
+    answer_type = setChoice(word_types(answer))
     misdirect_vocab = check_correct()
 
     while len(vocab_selection) < 3:
@@ -59,6 +65,7 @@ def answer_definition():
     return vocab_selection, answer, str(answer_def)
 
 def user_choice():
+    # This is good. Would you rather the user type out the answer as it is now, or select from a list (e.g. enter 1/2/3?
 
     vocab_selection, answer, answer_def = answer_definition()
     
@@ -73,6 +80,7 @@ def user_choice():
         return 1
 
     else:
+        # I loved this
         print("Incorect. For shame...")
         return 0
 
@@ -82,8 +90,10 @@ def start():
     while player_score < 3:
         print(f"Your score is currently {player_score}. You require 3 points to leave.")
         
+        # The interactive part of the game is fragmented across this function and user_choice. It might be easier to read if it was all in one place.
         player_score += user_choice()
 
+    # I never got here
     print("Congratualtions. You are a scholar!")
     quit()
 
