0de9dc55261a47e36a8a9f821deaa0cd573afad2
Eoin Groat <iam@eoingroat.com>
2024-11-22 14:52:22 +0000

    It was only the really old computers and languages that had character limits for variable names. Now we haven't had a meaningful character limit for a few decades - clear function names are much better!
    Whilst naming can get too long (average_secondary_derivatices_of_velocity_where_acceleration_exceeds_local_g) it is generally possible to find clear ways to name things.
    
    There is a famous saying in CS. "There are only two hard problems in programming - cache invalidation, naming things, and off by one errors"

--- user_vocab.py
+++ user_vocab.py
@@ -10,12 +10,12 @@ else:
     with open(filename) as f:
         content = f.read().splitlines()
 
-def rmv_non_charac(word):
+def remove_non_character(word):
     edited_word = [character for character in word if ord(character) in range (97, 123)]
     if len("".join(edited_word)) > 0:
         return "".join(edited_word) 
 
 unclean_vocab = sorted({words.lower() for words in content if " " not in words}) #issue here: as single words with spaces excluded
-clean_vocab = {rmv_non_charac(word) for word in unclean_vocab}
+clean_vocab = {remove_non_character(word) for word in unclean_vocab}
 
 #print("This is clean_vocab: ", clean_vocab)
