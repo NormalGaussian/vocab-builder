3d7604efd6d28f5a8742117dee3b6b1d64003a25
Eoin Groat <iam@eoingroat.com>
2024-11-22 15:28:32 +0000

    Way too many comments on the user_vocab file

--- user_vocab.py
+++ user_vocab.py
@@ -1,6 +1,22 @@
 import os.path
 
 
+# This file reading is good, but it can be done better.
+
+## Immediate things:
+# Sanitization can be done better; at the very least 
+#
+## Quick things:
+# Better comments about what is happening.
+# Add some context to the error messages - what file does not exist?
+# exit with a non-zero status code on error - this is a good practice and probably a new concept for you.
+#
+## Harder future things:
+# - consider offering an interface over a singleton
+# - we can look at logging in more detail
+# - consider caching.
+# - consider more complex techniques for parsing and cleaning data
+
 filename = "kindle_highlights.txt"
 
 if not os.path.isfile(filename):
@@ -11,13 +27,17 @@ else:
         content = f.read().splitlines()
 
 def remove_non_character(word):
+    # Whats going on here? What is happening is neither trivial nor obvious, thus it is prone to bugs.
+    # Also consider the name of your assigned variable; it is not a word, but a list of characters.
     edited_word = [character for character in word if ord(character) in range (97, 123)]
+    # `"".join(edited_word)`` repeated is a bit of a code smell; we can just do it once.
     if len("".join(edited_word)) > 0:
         return "".join(edited_word) 
     # This was implicit before; its not great to have a function `(str) -> str | None``
     return None
 
 # nb. sort was not necessary
+# nb. the two sets were not necessary
 
 clean_vocab = set()
 for line in content:
