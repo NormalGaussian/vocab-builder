509bbcfe9a8c62beb91dd48f55a660b83359769c
George Dean <georgedean27@gmail.com>
2024-11-26 08:58:11 +0000

    Made some basic changes based on Eoin's comments, including more specific error handling and reducing api calls.

--- get_defs.py
+++ get_defs.py
@@ -5,20 +5,22 @@ def api_call(word: str) -> list:
     
     trial = 0
     time.sleep(0.5)  #0.4 sleep with 2 attempts has had best results
+
     while trial < 2:
-        trial +=1
+
         try:
             url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
             response = requests.get(url)
 
             if response.status_code == 200:
                     data = response.json()
-                    #print(f"Data found for {word}")
-                    return data
-            
+                    return data          
             else:
-                print(f"This was the error code: {response.status_code}")
-        except:
+                trial +=1
+                raise Exception(f"non-200 status code returned for {word}: {response.status_code}")
+            
+
+        except Exception:
             print(f"Bad Request - no relvant url found for {word}")
             return None
     
@@ -26,19 +28,22 @@ def api_call(word: str) -> list:
 #Returns a set of all found vocab types for a given word (e.g. {"verb", "noun"})
 def word_types(word):
     
-    word_types = set({})
+    word_types = set()
     current_call = api_call(word)
     try:
         if current_call != None:
             for items in current_call:
                 for mean_items in items['meanings']:
                     word_types.add(mean_items['partOfSpeech'])
-            if len(list(word_types)) > 0:
+            if len((word_types)) > 0:
                 return word_types
-            else:
-                return None
-    except:
+
+    except IndexError:
+        print(f"Index Error raised in word_types for {word}.")
         return None
+    
+    else:
+        print(f"Non-index error raised in word_types for {word}.")
                     
 
 #Returns a list of all definitions for a given word OR definitions
@@ -62,13 +67,13 @@ def extract_defs(word, vocab_type = None):
         print(f"No definitions of type {vocab_type} found for {word}")
     
     #Returns all definiitions of all types
-    elif api_call(word) != None:
-        for items in api_call(word):
+    elif current_call != None:
+        for items in current_call:
             for mean_items in items['meanings']:
                 for spec_defs in mean_items['definitions']:
                     all_defs.append(spec_defs['definition'])
         if len(all_defs) > 0:
             return all_defs
-    
-    return None
 
+
+print(extract_defs('nascent', 'adjective'))
