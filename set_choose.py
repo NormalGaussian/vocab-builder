76842ca646b7993166f7c0fdbd45303eac27cbd4
thezeit <30597367+thezeit@users.noreply.github.com>
2024-11-27 15:30:24 +0000

    Add docstring to set choice
    Co-authored-by: Normal Gaussian <48660366+NormalGaussian@users.noreply.github.com>

--- set_choose.py
+++ set_choose.py
@@ -6,6 +6,11 @@ T = TypeVar('T')
 #To note - the index reference is not constant
 #due to set
 def setChoice(s: Set[T]) -> T | None:
+    """
+    Chooses a single element at random from a set.
+    When given the empty set returns None.
+    When given a non-empty set always returns a value.
+    """
     if len(s) == 0:
         return None
     index = random.randint(0, len(s) - 1)
